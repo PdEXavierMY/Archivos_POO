44f3f48991d9db573123da2fc235e81388ddf6ad
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-03 19:41:35 +0200

    test1

--- archivos.py
+++ archivos.py
@@ -1,11 +1,28 @@
-
 import csv
 
 def diccionarioprueba():
     with open("calificaciones.csv", newline='') as file:
-        reader = csv.reader(file)
+        reader = csv.reader(file, delimiter=';')
         for linea in reader:
             print(linea)
     file.close()
 
+def diccionario():
+    with open("calificaciones.csv", newline='') as file:
+        reader = csv.reader(file, delimiter=';')
+        lista = []
+        parametros = []
+        n = 0
+        for linea in reader:
+            n+=1
+            if n == 1:
+                for valor in linea:
+                    parametros.append(valor)
+            else:
+                for i in range(9):
+                    lista.append({parametros[i]:linea[i]})
+        print(lista)
+    file.close()
+
+diccionarioprueba()
 diccionario()
---
+++ prueba.py
@@ -0,0 +1,5 @@
+dic = {1: "a",
+        2:"b"}
+print(dic)
+dic.setdefault(3,"c")
+print(dic)
