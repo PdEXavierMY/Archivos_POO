2c9b86ba5688fb65b5b3b7b164e870d76006d894
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-04 19:57:12 +0200

    final parte 2

--- archivos.py
+++ archivos.py
@@ -43,16 +43,23 @@ def notafinal(lista):
         for i in range(3, 9):
             valores.append(dic[parametros[i]])
         notasporcentajes = []
-        if valores[0] != "":
-            if int(valores[0]) < 5:
-                if valores[2] != "":
-                    notasporcentajes.append(int(valores[2]))
-            else:
-                notasporcentajes.append(int(valores[0]))
-        else:
-            if valores[2] != "":
-                    notasporcentajes.append(int(valores[2]))
-            else:
-                notasporcentajes.append(0)
+        comprobarnumeros(valores, notasporcentajes, 0, 2)
+        comprobarnumeros(valores, notasporcentajes, 1, 3)
+        comprobarnumeros(valores, notasporcentajes, 4, 5)
         n = notasporcentajes[0]*0,3 + notasporcentajes[1]*0,3 + notasporcentajes[2]*0,4
         dic.setdefault("Nota Final", n)
+
+def comprobarnumeros(valores, notasporcentajes, a, b):
+    if valores[a] != "":
+            if int(valores[a]) < 5:
+                if valores[b] != "":
+                    notasporcentajes.append(int(valores[b]))
+                else:
+                    notasporcentajes.append(int(valores[a]))
+            else:
+                notasporcentajes.append(int(valores[a]))
+    else:
+        if valores[b] != "":
+                notasporcentajes.append(int(valores[b]))
+        else:
+            notasporcentajes.append(0)
