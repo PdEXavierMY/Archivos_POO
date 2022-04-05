c559225813b6ad4aa6a27604579e68d8a32a3edd
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-04-05 09:31:22 +0200

    mejoras varias

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from introducir import solicitar_introducir_numero
+from introducir import solicitar_introducir_numero, solicitar_introducir_numero_extremo
 from archivos import Calificaciones as C
 
 if __name__ == "__main__":
     elección = solicitar_introducir_numero("¿Desea mostrar la lista de diccionarios del archivo csv(1), mostrar la lista de diccionarios del archivo csv con las notas finales(2), o mostrar los alumnos aprobados y suspendos(3)?(cualquier otro número para salir)")
-    notas = C.diccionario()
-    notasord = C.ordenardic(notas)
-    notasfinales = C.notafinal(notasord)
     if elección == 1:
+        notas = C.diccionario()
         print("La lista de diccionarios tal cuál está el archivo es la siguiente:\n")
-        print(str(notas)+"\n")
+        for elemento in notas:
+            print(str(elemento)+"\n")
+        notasord = C.ordenardic(notas)
         print("La lista de diccionarios ordenada por apellidos es la siguiente:\n")
-        print(notasord+"\n")
+        for elemento in notasord:
+            print(str(notasord)+"\n")
     elif elección == 2:
+        notas = C.diccionario()
+        notasord = C.ordenardic(notas)
+        notasfinales = C.notafinal(notasord)
         print("La lista de diccionarios con las notas finales es la siguiente:\n")
-        print(notasfinales+"\n")
+        for elemento in notasfinales:
+            print(str(notasfinales)+"\n")
     elif elección == 3:
+        notas = C.diccionario()
+        notasord = C.ordenardic(notas)
+        notasfinales = C.notafinal(notasord)
         print("Los aprobados y suspensos siguiendo los criterios establecidos son los siguientes:\n")
-        C.apto(notasfinales+"\n")
+        C.apto(notasfinales)
     else:
         print("Ha seleccionado la opción de salir.")
